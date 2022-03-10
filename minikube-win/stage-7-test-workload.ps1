1b22fabea7db4b3e09056299b203f0013c44baab
Ben Wilcock <wilcockb@vmware.com>
2022-03-10 18:59:37 +0000

    fixed and issue after testing the beta of 1.1 of TAP

--- minikube-win/stage-7-test-workload.ps1
+++ minikube-win/stage-7-test-workload.ps1
@@ -9,9 +9,3 @@ curl.exe http://tanzu-java-web-app.default.apps.made-up-name.net
 
 # Final confirmation
 Write-Host "`n^^ Here you should see the words `"Greetings from Spring Boot + Tanzu!`" if the workload is responding." -ForegroundColor Blue -BackgroundColor Black
-
-# Test the application is responding (may take a few seconds at first)
-curl.exe http://tanzu-java-web-app.default.apps.made-up-name.net/actuator/health
-
-# Final confirmation
-Write-Host "`n^^ Here you should see the acutator/health information for the workload." -ForegroundColor Blue -BackgroundColor Black
