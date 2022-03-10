e80041cbc52c87c9265158f5c6229ed2ed41d13e
benwilcock <benwilcock@gmail.com>
2022-03-10 17:54:43 +0000

    fixed a bug in the create workspace

--- minikube-linux/stage-5-create-workspace.sh
+++ minikube-linux/stage-5-create-workspace.sh
@@ -12,19 +12,16 @@ source ./helper.sh
 yes_or_quit "$( echo -e "${NC}This script will create a developer workspace on TAP and add registry-credentials for $REPOSITORY_TYPE. Continue?" )"
 
 # Create a namespace for the developer to work in 
-kubectl create ns $TAP_DEV_NAMESPACE 
+kubectl create ns ${TAP_DEV_NAMESPACE} 
 
 # Add the secret for the BUILD Container Registry 
-tanzu secret registry add registry-credentials \
-  --server $DOCKER_SERVER \
-  --username $DOCKER_USERNAME \
-  --password $DOCKER_PASSWORD \
-  --namespace $TAP_DEV_NAMESPACE 
+tanzu secret registry add registry-credentials --server ${DOCKER_SERVER} --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD} --namespace ${TAP_DEV_NAMESPACE} 
+
 
 # Obtain the service accounts file 
-curl.exe -o serviceaccounts.yml https://raw.githubusercontent.com/benwilcock/tanzu-application-platform-scripts/main/minikube-win/serviceaccounts.yml 
+curl -o serviceaccounts.yml https://raw.githubusercontent.com/benwilcock/tanzu-application-platform-scripts/main/minikube-win/serviceaccounts.yml 
 
 # Add the necessary RBAC Roles, Accounts, Bindings etc... 
-kubectl -n $TAP_DEV_NAMESPACE apply -f "serviceaccounts.yml" 
+kubectl -n ${TAP_DEV_NAMESPACE} apply -f "serviceaccounts.yml" 
 
 echo -e "${GREEN}Next, run the stage-6 script.${NC}" 5
