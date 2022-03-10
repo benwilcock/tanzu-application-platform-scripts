12cea99d41ffd803bb671d2521f6bfc8d8f41509
benwilcock <benwilcock@gmail.com>
2022-03-10 16:12:55 +0000

    removed some old unused files

--- minikube-linux/create-minikube-cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-minikube start --cpus=8 --memory=16g --kubernetes-version='1.22.6'
-
-
--- minikube-linux/template-tap-env.sh
+++ minikube-linux/template-tap-env.sh
@@ -5,10 +5,7 @@ export INSTALL_REGISTRY_USERNAME="" # "tanzu-net-username"
 export INSTALL_REGISTRY_PASSWORD="" # "tanzu-net-password"
 
 # Built application image container registry (holds supply-chain container images)
-export DOCKER_SERVER="https://index.docker.io/v1/" # "or harbor.made-up-name.io"
 export DOCKER_USERNAME="" # "your-dockerhub-username"
 export DOCKER_PASSWORD="" # "your-dockerhub-password"
 
-# Login to Tanzu net (https://network.tanzu.vmware.com/) and get this API token from your profile page.
-export TANZU_NETWORK_TOKEN="" # Add your token here
 
